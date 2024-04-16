b11b2521c0a2e06fb0ebb65e14583664e62ae51d
Jonathan Moratelli de Carvalho <jonathan@grupoofs.com.br>
2024-04-16 09:44:38 -0300

    correções de nomes no BD

--- app/core/db/db.py
+++ app/core/db/db.py
@@ -1,17 +1,17 @@
 import sqlite3
 
-db_conn = sqlite3.connect('data/auth_server.db')
+db_conn = sqlite3.connect('data/auth_server.sqlite', check_same_thread=False)
 
 def init_db():
     cursor = db_conn.cursor()
     cursor.execute("""
         CREATE TABLE IF NOT EXISTS
         licenses(
-            licence_key     TEXT NOT NULL UNIQUE,
+            license_key     TEXT NOT NULL UNIQUE,
             machine_id  	TEXT,
             created_at      DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
             updated_at      DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
-            PRIMARY KEY("licence_key")
+            PRIMARY KEY("license_key")
         )
     """)
     cursor.close()
